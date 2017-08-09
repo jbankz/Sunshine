a676e4563ea3f12365788c4657feae19c42255e4
jaycee <vsquare396@gmail.com>
2017-08-09 15:36:03 -0700

    first commit

---
+++ app/src/main/java/sunshine/com/sync/SunshineSyncTask.java
@@ -0,0 +1,77 @@
+
+package sunshine.com.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.text.format.DateUtils;
+
+import java.net.URL;
+
+import sunshine.com.data.SunshinePreferences;
+import sunshine.com.data.WeatherContract;
+import sunshine.com.utilities.NetworkUtils;
+import sunshine.com.utilities.NotificationUtils;
+import sunshine.com.utilities.OpenWeatherJsonUtils;
+
+public class SunshineSyncTask {
+
+    synchronized public static void syncWeather(Context context) {
+
+        try {
+
+            URL weatherRequestUrl = NetworkUtils.getUrl(context);
+
+            /* Use the URL to retrieve the JSON */
+            String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+
+            /* Parse the JSON into a list of weather values */
+            ContentValues[] weatherValues = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+            if (weatherValues != null && weatherValues.length != 0) {
+                /* Get a handle on the ContentResolver to delete and insert data */
+                ContentResolver sunshineContentResolver = context.getContentResolver();
+
+                /* Delete old weather data because we don't need to keep multiple days' data */
+                sunshineContentResolver.delete(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        null,
+                        null);
+
+                /* Insert our new weather data into Sunshine's ContentProvider */
+                sunshineContentResolver.bulkInsert(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        weatherValues);
+
+                /*
+                 * Finally, after we insert data into the ContentProvider, determine whether or not
+                 * we should notify the user that the weather has been refreshed.
+                 */
+                boolean notificationsEnabled = SunshinePreferences.areNotificationsEnabled(context);
+
+
+                long timeSinceLastNotification = SunshinePreferences
+                        .getEllapsedTimeSinceLastNotification(context);
+
+                boolean oneDayPassedSinceLastNotification = false;
+
+                if (timeSinceLastNotification >= DateUtils.DAY_IN_MILLIS) {
+                    oneDayPassedSinceLastNotification = true;
+                }
+
+
+                if (notificationsEnabled && oneDayPassedSinceLastNotification) {
+                    NotificationUtils.notifyUserOfNewWeather(context);
+                }
+
+            /* If the code reaches this point, we have successfully performed our sync */
+
+            }
+
+        } catch (Exception e) {
+            /* Server probably invalid */
+            e.printStackTrace();
+        }
+    }
+}
